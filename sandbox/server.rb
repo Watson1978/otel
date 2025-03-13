60df987273176c13c8b2eceb6ddde9abe55679db
Shizuo Fujita <fujita@clear-code.com>
2025-03-13 13:27:57 +0900

    generate with grpc_tools_ruby_protoc

--- sandbox/server.rb
+++ sandbox/server.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'sinatra'
 require 'google/protobuf'
+require './opentelemetry/proto/collector/metrics/v1/metrics_service_pb.rb'
 require './opentelemetry/proto/metrics/v1/metrics_pb.rb'
 
 
@@ -8,7 +9,9 @@ require './opentelemetry/proto/metrics/v1/metrics_pb.rb'
 post '/v1/metrics' do
   p "!" * 80
   data = request.body.read
-  proto = Opentelemetry::Proto::Metrics::V1::MetricsData.decode(data)
+  p data
+  proto = Opentelemetry::Proto::Collector::Metrics::V1::ExportMetricsServiceRequest.decode(data)
   p proto
-  p Opentelemetry::Proto::Metrics::V1::MetricsData.decode_json(proto.to_json)
+  p proto.to_json
+  p Opentelemetry::Proto::Collector::Metrics::V1::ExportMetricsServiceRequest.decode_json(proto.to_json)
 end
